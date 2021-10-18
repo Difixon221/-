331a9ab09c6f59e8145991a504c95566cd460373
usersql <usersql@PK306NEW-3>
2021-10-18 13:37:30 +0500

    ShipDate

--- Ships2.0/Form1.Designer.cs
+++ Ships2.0/Form1.Designer.cs
@@ -28,13 +28,213 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.Battles = new System.Windows.Forms.DataGridView();
+            this.Column1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Classes = new System.Windows.Forms.DataGridView();
+            this.dataGridViewTextBoxColumn1 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn2 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Outcomes = new System.Windows.Forms.DataGridView();
+            this.dataGridViewTextBoxColumn3 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn4 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column7 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Ships = new System.Windows.Forms.DataGridView();
+            this.dataGridViewTextBoxColumn5 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.dataGridViewTextBoxColumn6 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.Column8 = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.Battles)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Classes)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Outcomes)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Ships)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "Battles",
+            "Classes",
+            "Outcomes",
+            "Ships"});
+            this.comboBox1.Location = new System.Drawing.Point(12, 12);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // Battles
+            // 
+            this.Battles.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.Battles.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.Column1,
+            this.Column2});
+            this.Battles.Location = new System.Drawing.Point(12, 39);
+            this.Battles.Name = "Battles";
+            this.Battles.Size = new System.Drawing.Size(776, 399);
+            this.Battles.TabIndex = 1;
+            this.Battles.Visible = false;
+            // 
+            // Column1
+            // 
+            this.Column1.HeaderText = "name";
+            this.Column1.Name = "Column1";
+            // 
+            // Column2
+            // 
+            this.Column2.HeaderText = "date";
+            this.Column2.Name = "Column2";
+            // 
+            // Classes
+            // 
+            this.Classes.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.Classes.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dataGridViewTextBoxColumn1,
+            this.dataGridViewTextBoxColumn2,
+            this.Column3,
+            this.Column4,
+            this.Column5,
+            this.Column6});
+            this.Classes.Location = new System.Drawing.Point(12, 39);
+            this.Classes.Name = "Classes";
+            this.Classes.Size = new System.Drawing.Size(776, 399);
+            this.Classes.TabIndex = 2;
+            this.Classes.Visible = false;
+            // 
+            // dataGridViewTextBoxColumn1
+            // 
+            this.dataGridViewTextBoxColumn1.HeaderText = "class";
+            this.dataGridViewTextBoxColumn1.Name = "dataGridViewTextBoxColumn1";
+            // 
+            // dataGridViewTextBoxColumn2
+            // 
+            this.dataGridViewTextBoxColumn2.HeaderText = "type";
+            this.dataGridViewTextBoxColumn2.Name = "dataGridViewTextBoxColumn2";
+            // 
+            // Column3
+            // 
+            this.Column3.HeaderText = "country";
+            this.Column3.Name = "Column3";
+            // 
+            // Column4
+            // 
+            this.Column4.HeaderText = "numGuns";
+            this.Column4.Name = "Column4";
+            // 
+            // Column5
+            // 
+            this.Column5.HeaderText = "bore";
+            this.Column5.Name = "Column5";
+            // 
+            // Column6
+            // 
+            this.Column6.HeaderText = "displacement";
+            this.Column6.Name = "Column6";
+            // 
+            // Outcomes
+            // 
+            this.Outcomes.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.Outcomes.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dataGridViewTextBoxColumn3,
+            this.dataGridViewTextBoxColumn4,
+            this.Column7});
+            this.Outcomes.Location = new System.Drawing.Point(12, 39);
+            this.Outcomes.Name = "Outcomes";
+            this.Outcomes.Size = new System.Drawing.Size(776, 399);
+            this.Outcomes.TabIndex = 3;
+            this.Outcomes.Visible = false;
+            // 
+            // dataGridViewTextBoxColumn3
+            // 
+            this.dataGridViewTextBoxColumn3.HeaderText = "ship";
+            this.dataGridViewTextBoxColumn3.Name = "dataGridViewTextBoxColumn3";
+            // 
+            // dataGridViewTextBoxColumn4
+            // 
+            this.dataGridViewTextBoxColumn4.HeaderText = "battle";
+            this.dataGridViewTextBoxColumn4.Name = "dataGridViewTextBoxColumn4";
+            // 
+            // Column7
+            // 
+            this.Column7.HeaderText = "result";
+            this.Column7.Name = "Column7";
+            // 
+            // Ships
+            // 
+            this.Ships.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.Ships.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
+            this.dataGridViewTextBoxColumn5,
+            this.dataGridViewTextBoxColumn6,
+            this.Column8});
+            this.Ships.Location = new System.Drawing.Point(12, 39);
+            this.Ships.Name = "Ships";
+            this.Ships.Size = new System.Drawing.Size(776, 399);
+            this.Ships.TabIndex = 4;
+            this.Ships.Visible = false;
+            // 
+            // dataGridViewTextBoxColumn5
+            // 
+            this.dataGridViewTextBoxColumn5.HeaderText = "name";
+            this.dataGridViewTextBoxColumn5.Name = "dataGridViewTextBoxColumn5";
+            // 
+            // dataGridViewTextBoxColumn6
+            // 
+            this.dataGridViewTextBoxColumn6.HeaderText = "class";
+            this.dataGridViewTextBoxColumn6.Name = "dataGridViewTextBoxColumn6";
+            // 
+            // Column8
+            // 
+            this.Column8.HeaderText = "launched";
+            this.Column8.Name = "Column8";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.Controls.Add(this.Ships);
+            this.Controls.Add(this.Outcomes);
+            this.Controls.Add(this.Classes);
+            this.Controls.Add(this.Battles);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Главная";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.Battles)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Classes)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Outcomes)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Ships)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.DataGridView Battles;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column2;
+        private System.Windows.Forms.DataGridView Classes;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn1;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn2;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column6;
+        private System.Windows.Forms.DataGridView Outcomes;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn3;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn4;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column7;
+        private System.Windows.Forms.DataGridView Ships;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn5;
+        private System.Windows.Forms.DataGridViewTextBoxColumn dataGridViewTextBoxColumn6;
+        private System.Windows.Forms.DataGridViewTextBoxColumn Column8;
     }
 }
 
--- Ships2.0/Form1.cs
+++ Ships2.0/Form1.cs
@@ -1,20 +1,111 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace Ships2._0
 {
     public partial class Form1 : Form
-    {
+    {123
+        public static string Connection = @"Data Source=PK306NEW-10;Initial Catalog=Ships;Integrated Security=True";
+        private SqlConnection sqlConnection = new SqlConnection(Connection);
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            sqlConnection.Open();
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if(comboBox1.SelectedIndex == 0)
+            {
+                Battles.Visible = true;
+                Classes.Visible = false;
+                Outcomes.Visible = false;
+                Ships.Visible = false;
+                BattlesInfo();
+            }
+            else if (comboBox1.SelectedIndex == 1)
+            {
+                Battles.Visible = false;
+                Classes.Visible = true;
+                Outcomes.Visible = false;
+                Ships.Visible = false;
+                ClassesInfo();
+            }
+            else if (comboBox1.SelectedIndex == 2)
+            {
+                Battles.Visible = false;
+                Classes.Visible = false;
+                Outcomes.Visible = true;
+                Ships.Visible = false;
+                OutcomesInfo();
+            }
+            else if (comboBox1.SelectedIndex == 3)
+            {
+                Battles.Visible = false;
+                Classes.Visible = false;
+                Outcomes.Visible = false;
+                Ships.Visible = true;
+                ShipsInfo();
+            }
+
+        }
+        public void BattlesInfo()
+        {
+            Battles.Rows.Clear();
+            SqlCommand sqlCommand = new SqlCommand();
+            sqlCommand.Connection = sqlConnection;
+            sqlCommand.CommandText = $"Select * from Battles";
+            SqlDataReader reader = sqlCommand.ExecuteReader();
+            while (reader.Read())
+            {
+                Battles.Rows.Add(reader[0].ToString(), reader[1].ToString());
+            }
+            reader.Close();
+        }
+        public void ClassesInfo()
+        {
+            Classes.Rows.Clear();
+            SqlCommand sqlCommand = new SqlCommand();
+            sqlCommand.Connection = sqlConnection;
+            sqlCommand.CommandText = $"Select * from Classes";
+            SqlDataReader reader = sqlCommand.ExecuteReader();
+            while (reader.Read())
+            {
+                Classes.Rows.Add(reader[0].ToString(), reader[1].ToString(), reader[2].ToString(), reader[3].ToString(), reader[4].ToString(), reader[5].ToString());
+            }
+            reader.Close();
+        }
+        public void OutcomesInfo()
+        {
+            Outcomes.Rows.Clear();
+            SqlCommand sqlCommand = new SqlCommand();
+            sqlCommand.Connection = sqlConnection;
+            sqlCommand.CommandText = $"Select * from Outcomes";
+            SqlDataReader reader = sqlCommand.ExecuteReader();
+            while (reader.Read())
+            {
+                Outcomes.Rows.Add(reader[0].ToString(), reader[1].ToString(), reader[2].ToString());
+            }
+            reader.Close();
+        }
+        public void ShipsInfo()
+        {
+            Ships.Rows.Clear();
+            SqlCommand sqlCommand = new SqlCommand();
+            sqlCommand.Connection = sqlConnection;
+            sqlCommand.CommandText = $"Select * from Ships";
+            SqlDataReader reader = sqlCommand.ExecuteReader();
+            while (reader.Read())
+            {
+                Ships.Rows.Add(reader[0].ToString(), reader[1].ToString(), reader[2].ToString());
+            }
+            reader.Close();
         }
     }
 }
